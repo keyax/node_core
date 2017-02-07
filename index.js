9c95326e9f33ca55d248357362a72d29958c4d66
keyax <keyax@live.com>
2017-02-07 13:28:14 +0000

    version 2.061

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require("http");
 var server = http.createServer(function(request, response) {
  response.writeHead(200, {"Content-Type": "text/html"});
- response.write("<!DOCTYPE "html">");
+ response.write("<!DOCTYPE html>");
  response.write("<html>");
  response.write("<head>");
  response.write("<title>Hello World Page</title>");
